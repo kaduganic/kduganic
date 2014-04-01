e3d2a93ec3a3f4a10d6d00dac5a8e08f0f7c8d3a
Karlo Duganić <karloduganic@gmail.com>
2014-04-01 11:28:58 +0200

    zadnji
    zadnji

--- kduganic/Form1.Designer.cs
+++ kduganic/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace kduganic
 {
-    partial class Form1
+    partial class frmGlavna
     {
         /// <summary>
         /// Required designer variable.
@@ -28,12 +28,149 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.lblPolumjer = new System.Windows.Forms.Label();
+            this.lblPovrsina = new System.Windows.Forms.Label();
+            this.lblOpseg = new System.Windows.Forms.Label();
+            this.txtPolumjer = new System.Windows.Forms.TextBox();
+            this.txtPovrsina = new System.Windows.Forms.TextBox();
+            this.txtOpseg = new System.Windows.Forms.TextBox();
+            this.chkSpremi = new System.Windows.Forms.CheckBox();
+            this.lstRezultati = new System.Windows.Forms.ListBox();
+            this.btnIzracunaj = new System.Windows.Forms.Button();
+            this.btnOcisti = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblPolumjer
+            // 
+            this.lblPolumjer.AutoSize = true;
+            this.lblPolumjer.Location = new System.Drawing.Point(22, 17);
+            this.lblPolumjer.Name = "lblPolumjer";
+            this.lblPolumjer.Size = new System.Drawing.Size(50, 13);
+            this.lblPolumjer.TabIndex = 0;
+            this.lblPolumjer.Text = "Polumjer:";
+            this.lblPolumjer.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // lblPovrsina
+            // 
+            this.lblPovrsina.AutoSize = true;
+            this.lblPovrsina.Location = new System.Drawing.Point(22, 55);
+            this.lblPovrsina.Name = "lblPovrsina";
+            this.lblPovrsina.Size = new System.Drawing.Size(48, 13);
+            this.lblPovrsina.TabIndex = 1;
+            this.lblPovrsina.Text = "Površina";
+            // 
+            // lblOpseg
+            // 
+            this.lblOpseg.AutoSize = true;
+            this.lblOpseg.Location = new System.Drawing.Point(22, 94);
+            this.lblOpseg.Name = "lblOpseg";
+            this.lblOpseg.Size = new System.Drawing.Size(41, 13);
+            this.lblOpseg.TabIndex = 2;
+            this.lblOpseg.Text = "Opseg:";
+            this.lblOpseg.Click += new System.EventHandler(this.label1_Click_1);
+            // 
+            // txtPolumjer
+            // 
+            this.txtPolumjer.Location = new System.Drawing.Point(83, 17);
+            this.txtPolumjer.Name = "txtPolumjer";
+            this.txtPolumjer.Size = new System.Drawing.Size(100, 20);
+            this.txtPolumjer.TabIndex = 3;
+            this.txtPolumjer.Text = "0,00";
+            this.txtPolumjer.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // txtPovrsina
+            // 
+            this.txtPovrsina.Location = new System.Drawing.Point(83, 55);
+            this.txtPovrsina.Name = "txtPovrsina";
+            this.txtPovrsina.Size = new System.Drawing.Size(100, 20);
+            this.txtPovrsina.TabIndex = 4;
+            this.txtPovrsina.Text = "0,00";
+            this.txtPovrsina.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // txtOpseg
+            // 
+            this.txtOpseg.Location = new System.Drawing.Point(83, 94);
+            this.txtOpseg.Name = "txtOpseg";
+            this.txtOpseg.Size = new System.Drawing.Size(100, 20);
+            this.txtOpseg.TabIndex = 5;
+            this.txtOpseg.Text = "0,00";
+            this.txtOpseg.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // chkSpremi
+            // 
+            this.chkSpremi.AutoSize = true;
+            this.chkSpremi.Location = new System.Drawing.Point(83, 126);
+            this.chkSpremi.Name = "chkSpremi";
+            this.chkSpremi.Size = new System.Drawing.Size(88, 17);
+            this.chkSpremi.TabIndex = 6;
+            this.chkSpremi.Text = "Spremi u listu";
+            this.chkSpremi.UseVisualStyleBackColor = true;
+            this.chkSpremi.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // lstRezultati
+            // 
+            this.lstRezultati.FormattingEnabled = true;
+            this.lstRezultati.Location = new System.Drawing.Point(235, 17);
+            this.lstRezultati.Name = "lstRezultati";
+            this.lstRezultati.Size = new System.Drawing.Size(120, 95);
+            this.lstRezultati.TabIndex = 7;
+            this.lstRezultati.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.lstRezultati_MouseDoubleClick);
+            // 
+            // btnIzracunaj
+            // 
+            this.btnIzracunaj.Location = new System.Drawing.Point(83, 149);
+            this.btnIzracunaj.Name = "btnIzracunaj";
+            this.btnIzracunaj.Size = new System.Drawing.Size(75, 23);
+            this.btnIzracunaj.TabIndex = 8;
+            this.btnIzracunaj.Text = "Izračunaj";
+            this.btnIzracunaj.UseVisualStyleBackColor = true;
+            this.btnIzracunaj.Click += new System.EventHandler(this.btnIzracunaj_Click);
+            // 
+            // btnOcisti
+            // 
+            this.btnOcisti.Location = new System.Drawing.Point(83, 178);
+            this.btnOcisti.Name = "btnOcisti";
+            this.btnOcisti.Size = new System.Drawing.Size(75, 23);
+            this.btnOcisti.TabIndex = 9;
+            this.btnOcisti.Text = "Očisti";
+            this.btnOcisti.UseVisualStyleBackColor = true;
+            this.btnOcisti.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // frmGlavna
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(384, 212);
+            this.Controls.Add(this.btnOcisti);
+            this.Controls.Add(this.btnIzracunaj);
+            this.Controls.Add(this.lstRezultati);
+            this.Controls.Add(this.chkSpremi);
+            this.Controls.Add(this.txtOpseg);
+            this.Controls.Add(this.txtPovrsina);
+            this.Controls.Add(this.txtPolumjer);
+            this.Controls.Add(this.lblOpseg);
+            this.Controls.Add(this.lblPovrsina);
+            this.Controls.Add(this.lblPolumjer);
+            this.Name = "frmGlavna";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Glavna forma aplikacije";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lblPolumjer;
+        private System.Windows.Forms.Label lblPovrsina;
+        private System.Windows.Forms.Label lblOpseg;
+        private System.Windows.Forms.TextBox txtPolumjer;
+        private System.Windows.Forms.TextBox txtPovrsina;
+        private System.Windows.Forms.TextBox txtOpseg;
+        private System.Windows.Forms.CheckBox chkSpremi;
+        private System.Windows.Forms.ListBox lstRezultati;
+        private System.Windows.Forms.Button btnIzracunaj;
+        private System.Windows.Forms.Button btnOcisti;
     }
 }
 
--- kduganic/Form1.cs
+++ kduganic/Form1.cs
@@ -9,11 +9,68 @@ using System.Windows.Forms;
 
 namespace kduganic
 {
-    public partial class Form1 : Form
+    public partial class frmGlavna : Form
     {
-        public Form1()
+        public frmGlavna()
         {
             InitializeComponent();
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+
+        private void btnIzracunaj_Click(object sender, EventArgs e)
+        {
+            float polumjer = 0;
+            float povrsina = 0;
+            float opseg = 0;
+
+            float.TryParse(txtPolumjer.Text, out polumjer);
+
+            izracun(polumjer);
+
+            if (chkSpremi.Checked == true) lstRezultati.Items.Add(polumjer);
+            
+
+        }
+
+        private void izracun(float polumjer)
+        {
+            float povrsina = 0;
+            float opseg = 0;
+            if (polumjer > 0)
+            {
+                povrsina = polumjer * polumjer * (float)Math.PI;
+                opseg = 2 * polumjer * (float)Math.PI;
+
+                txtPovrsina.Text = povrsina.ToString();
+                txtOpseg.Text = opseg.ToString();
+            }
+        }
+
+        private void lstRezultati_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            float polumjer = 0;
+
+            float.TryParse(lstRezultati.SelectedItem.ToString(), out polumjer);
+            txtPolumjer.Text = polumjer.ToString();
+
+            izracun(polumjer);
+        }
+
+      
     }
 }
